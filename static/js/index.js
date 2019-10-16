d85960ba30c000ce196db4b5afd13fe2bd0d6675
xyy <1499488968@qq.com>
2019-10-16 21:24:41 +0800

    Initial commit

---
+++ static/js/index.js
@@ -0,0 +1,47 @@
+// 显示选择的图片
+$('#image').change(function () {
+    let image = this.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = function () {
+        $('#choose-img').attr('src', this.result);
+    }
+});
+
+
+// 显示选择的风格图片
+$('#choice').change(function () {
+    let img_path = '/static/img/' + $('#choice').val() + '.png';
+    $('#style-img').attr('src', img_path);
+});
+
+
+// 上传生成图片
+$('#submit').click(function () {
+    let formData = new FormData();
+    let image = $('#image')[0].files[0];
+    let choice = $('#choice').val();
+    formData.append('image', image);
+    formData.append('choice', choice);
+
+    $.ajax({
+        url: '/upload-image/',
+        type: 'POST',
+        dataType: "json",//预期服务器返回的数据类型
+        data: formData,
+        processData: false,
+        contentType: false,
+
+        success: function (data) {
+            alert(data['message']);
+            if (data['status'] == '0') {
+                let path = '/static/result/'
+                $('#result').attr('src', path + data['result'])
+            }
+        },
+
+        error: function () {
+
+        },
+    });
+});
